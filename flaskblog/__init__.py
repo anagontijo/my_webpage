b26e15de248a561d98ee18033cea87779be94984
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-30 21:20:31 -0300

    Mudanças para app

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = '98e17485c227c59e41d926802c3eb65c'
 
-db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
