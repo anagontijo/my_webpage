73d8a532b3265f454ce1993fd5c26d67e63faac9
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-10-30 21:29:16 -0300

    .

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,7 @@
 from flask import Flask
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '98e17485c227c59e41d926802c3eb65c'
-
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
 
 from flaskblog import routes
