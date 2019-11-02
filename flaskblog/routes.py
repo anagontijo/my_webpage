c63f2ddff0bec7b16af549f6ac3d0a42a937812b
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-11-01 22:52:45 -0300

    .

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -18,11 +18,41 @@ def get_courses():
     for sem in range(8):
         courses.append([])
     with open(os.getcwd()+"/flaskblog/static/docs/course.csv") as csvfile:
-        for row in csv.reader(csvfile, delimiter=',', quotechar='|'):
+        for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
             courses[int(row[0])-1].append([])
             for elem in range(4):
                 courses[int(row[0])-1][-1].append(row[elem+1])
     return courses
+
+def get_ext_courses():
+    ext_courses = []
+    with open(os.getcwd()+"/flaskblog/static/docs/ext_courses.csv") as csvfile:
+        for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
+            ext_courses.append([])
+            parts = row[5].split("/")
+            topics = []
+            for part in parts:
+                topics.append(part.split(":"))
+            for elem in range(5):
+                ext_courses[-1].append(row[elem])
+            ext_courses[-1].append(topics)
+            ext_courses[-1].append(row[6])
+    return ext_courses
+
+def get_jobs():
+    internships = []
+    jobs = []
+    with open(os.getcwd()+"/flaskblog/static/docs/jobs.csv") as csvfile:
+        for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
+            if int(row[0]) == 0:
+                internships.append([])
+                for elem in range(7):
+                    internships[-1].append(row[elem+1])
+            else:
+                jobs.append([])
+                for elem in range(7):
+                    jobs[-1].append(row[elem+1])
+    return internships, jobs
 
 # Rota para a página home do sistema
 @app.route("/")
@@ -36,16 +66,35 @@ def home():
 @app.route("/about")
 def about():
     logo_files, image_file = get_files()
+    courses = get_courses()
     return render_template("about.html", title='Sobre mim',
                            image_file = image_file,
                            logo_files = logo_files)
 
 @app.route("/studies")
 def studies():
-    courses = get_courses()
-    print(courses)
     logo_files, image_file = get_files()
-    return render_template("studies.html", title='Grad',
+    courses = get_courses()
+    return render_template("studies.html", title='Graduação',
                            image_file = image_file,
                            logo_files = logo_files,
-                           courses=courses)
+                           courses = courses)
+
+@app.route("/external-courses")
+def ext_courses():
+    logo_files, image_file = get_files()
+    ext_courses = get_ext_courses()
+    return render_template("external_courses.html", title='Cursos externos',
+                           logo_files = logo_files,
+                           image_file = image_file,
+                           ext_courses = ext_courses)
+
+@app.route("/jobs")
+def jobs():
+    logo_files, image_file = get_files()
+    internships, jobs = get_jobs()
+    return render_template("jobs.html", title='Experiência',
+                           logo_files = logo_files,
+                           image_file = image_file,
+                           internships = internships,
+                           jobs = jobs)
