0eb09ae771bc662441bd15f699734c059b6580ea
Ana Carolina Gontijo Graça <anagontijograca@gmail.com>
2019-11-10 02:57:00 -0200

    .

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '98e17485c227c59e41d926802c3eb65c'
-
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -5,8 +5,11 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app
 import csv
 
+class GlobalStore:
+    language = "pt"
+
 def get_files():
-    logos = ["document" ,"linkedin", "github", "trello", "gmail", "telegram","UFMG"]
+    logos = ["br","uk","document" ,"linkedin", "github", "trello", "gmail", "telegram","UFMG"]
     logo_files = {}
     for logo in logos:
         logo_files[logo] = url_for('static', filename='logos/'+logo+'.png')
@@ -17,7 +20,11 @@ def get_courses():
     courses = []
     for sem in range(8):
         courses.append([])
-    with open(os.getcwd()+"/flaskblog/static/docs/course.csv") as csvfile:
+    if variables.language == "pt":
+        path = "/flaskblog/static/docs/course_pt.csv"
+    else:
+        path = "/flaskblog/static/docs/course_en.csv"
+    with open(os.getcwd()+path) as csvfile:
         for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
             courses[int(row[0])-1].append([])
             for elem in range(4):
@@ -26,7 +33,11 @@ def get_courses():
 
 def get_ext_courses():
     ext_courses = []
-    with open(os.getcwd()+"/flaskblog/static/docs/ext_courses.csv") as csvfile:
+    if variables.language == "pt":
+        path = "/flaskblog/static/docs/ext_courses_pt.csv"
+    else:
+        path = "/flaskblog/static/docs/ext_courses_en.csv"
+    with open(os.getcwd()+path) as csvfile:
         for row in csv.reader(csvfile, delimiter=',', quotechar='"'):
             ext_courses.append([])
             parts = row[5].split("/")
@@ -38,6 +49,8 @@ def get_ext_courses():
             ext_courses[-1].append(topics)
             ext_courses[-1].append(row[6])
     return ext_courses
+
+variables = GlobalStore()
 
 def get_jobs():
     internships = []
@@ -68,6 +81,8 @@ def get_projects():
             projects[-1]["github"] = row[2]
             projects[-1]["name"] = row[1]
             projects[-1]["description"] = row[3]
+            projects[-1]["poster"] = url_for('static', filename='docs/'+row[4])
+            projects[-1]["poster_type"] = row[5]
 
     return projects
 
@@ -81,64 +96,107 @@ def get_cv_paths():
     return cv_path_en, cv_path_pt
 
 # Rota para a página home do sistema
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["POST","GET"])
+@app.route("/home", methods=["POST","GET"])
 def home():
+    if "english.x" in request.form:
+        variables.language = "en"
+    elif "portuguese.x" in request.form:
+        variables.language = "pt"
+
     logo_files, image_file = get_files()
     return render_template("home.html", image_file = image_file,
                            logo_files = logo_files,
-                           stylesheet = get_stylesheet())
+                           stylesheet = get_stylesheet(),
+                           language = variables.language,
+                           self_url = url_for("home"))
 
 # Rota para a página about do sistema
-@app.route("/about")
+@app.route("/about", methods=["POST","GET"])
 def about():
+    if "english.x" in request.form:
+        variables.language = "en"
+    elif "portuguese.x" in request.form:
+        variables.language = "pt"
+
     logo_files, image_file = get_files()
     courses = get_courses()
     cv_path_en, cv_path_pt = get_cv_paths()
-    return render_template("about.html", title='Sobre mim',
+
+    return render_template("about.html", title={"pt":"Sobre mim", "en":"About"},
                            image_file = image_file,
                            logo_files = logo_files,
                            stylesheet = get_stylesheet(),
                            cv_path_en = cv_path_en,
-                           cv_path_pt = cv_path_pt)
+                           cv_path_pt = cv_path_pt,
+                           language = variables.language,
+                           self_url = url_for("about"))
 
-@app.route("/studies")
+@app.route("/studies", methods=["POST","GET"])
 def studies():
+    if "english.x" in request.form:
+        variables.language = "en"
+    elif "portuguese.x" in request.form:
+        variables.language = "pt"
+
     logo_files, image_file = get_files()
     courses = get_courses()
-    return render_template("studies.html", title='Graduação',
+    return render_template("studies.html", title={"pt":"Graduação","en":"Studies"},
                            image_file = image_file,
                            logo_files = logo_files,
                            courses = courses,
-                           stylesheet = get_stylesheet())
+                           stylesheet = get_stylesheet(),
+                           language = variables.language,
+                           self_url = url_for("studies"))
 
-@app.route("/external-courses")
+@app.route("/external-courses", methods=["POST","GET"])
 def ext_courses():
+    if "english.x" in request.form:
+        variables.language = "en"
+    elif "portuguese.x" in request.form:
+        variables.language = "pt"
+
     logo_files, image_file = get_files()
-    ext_courses = get_ext_courses()
-    return render_template("external_courses.html", title='Cursos externos',
+    extern_courses = get_ext_courses()
+    return render_template("external_courses.html", title={"pt":"Cursos Externos","en":"External Courses"},
                            logo_files = logo_files,
                            image_file = image_file,
-                           ext_courses = ext_courses,
-                           stylesheet = get_stylesheet())
+                           ext_courses = extern_courses,
+                           stylesheet = get_stylesheet(),
+                           language = variables.language,
+                           self_url = url_for("ext_courses"))
 
-@app.route("/jobs")
+@app.route("/jobs", methods=["POST","GET"])
 def jobs():
+    if "english.x" in request.form:
+        variables.language = "en"
+    elif "portuguese.x" in request.form:
+        variables.language = "pt"
+
     logo_files, image_file = get_files()
     internships, jobs = get_jobs()
-    return render_template("jobs.html", title='Experiência',
+    return render_template("jobs.html", title={"pt":"Experiência","en":"Work Experience"},
                            logo_files = logo_files,
                            image_file = image_file,
                            internships = internships,
                            jobs = jobs,
-                           stylesheet = get_stylesheet())
+                           stylesheet = get_stylesheet(),
+                           language = variables.language,
+                           self_url = url_for("jobs"))
 
-@app.route("/projects")
+@app.route("/projects", methods=["POST","GET"])
 def projects():
+    if "english.x" in request.form:
+        variables.language = "en"
+    elif "portuguese.x" in request.form:
+        variables.language = "pt"
+
     logo_files, image_file = get_files()
     projects = get_projects()
-    return render_template("projects.html", title='Projetos',
+    return render_template("projects.html", title={"pt":"Projetos","en":"Projects"},
                            logo_files = logo_files,
                            image_file = image_file,
                            projects = projects,
-                           stylesheet = get_stylesheet())
+                           stylesheet = get_stylesheet(),
+                           language = variables.language,
+                           self_url = url_for("projects"))
